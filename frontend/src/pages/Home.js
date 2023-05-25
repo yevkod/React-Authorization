6eb97983335c63410a487334e7ec093db4368100
Yevgen <evgennevidim@gmail.com>
2023-05-25 10:36:04 +0300

    first commit

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,12 @@
+import PageContent from '../components/PageContent';
+
+function HomePage() {
+  return (
+    <PageContent title="Welcome!">
+      <p>Browse all our amazing events!</p>
+      <p>Please register to enjoy all features!</p>
+    </PageContent>
+  );
+}
+
+export default HomePage;
